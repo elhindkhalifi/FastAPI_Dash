09c3584f30bd6cec0729aea6faefc49f206c5e86
elhindkhalifi <elhindkhalifi@gmail.com>
2024-10-07 14:47:58 -0400

    another startup change for debugging

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 # move into the correct the directory where the fastAPI app is located 
-cd FastAPI_Dash
+# cd FastAPI_Dash
 
 # Ruun the FastAPI+Dash app using Uvicorn 
 uvicorn fastapi_app.main:app --host 0.0.0.0  --workers 1
