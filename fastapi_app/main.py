359c7e1a9a8d8751081cadf854f8cca2a29a2319
elhindkhalifi <elhindkhalifi@gmail.com>
2024-10-07 15:10:16 -0400

    startup change for debugging

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -57,5 +57,5 @@ async def logout():
 app.mount("/dashboard", WSGIMiddleware(app_dash.server))
 
 if __name__=='__main__':
-    uvicorn.run(app,host='0.0.0.0', port=8001, workers=1)
+    uvicorn.run(app,host='0.0.0.0', port=8021, workers=1)
     
